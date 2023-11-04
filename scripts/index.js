c48417464b5b8ed02e897dd2ae23854a6556333a
Jeremias Gunsett <jeremias.gunsett@itscordoba.edu.ar>
2023-11-04 15:08:07 -0300

    Se agrega primer molde de carrusel de categorias en el index

--- scripts/index.js
+++ scripts/index.js
@@ -1,72 +1,37 @@
-class Carousel {
-    constructor(items, active) {
-        this.items = items;
-        this.active = active;
-        this.direction = '';
-        this.rightClick = this.moveRight.bind(this);
-        this.leftClick = this.moveLeft.bind(this);
-        this.render();
-    }
 
-    generateItems() {
-        var items = [];
-        var level;
-        console.log(this.active);
-        for (var i = this.active - 2; i < this.active + 3; i++) {
-            var index = i;
-            if (i < 0) {
-                index = this.items.length + i;
-            } else if (i >= this.items.length) {
-                index = i % this.items.length;
-            }
-            level = this.active - i;
-            items.push(new Item(this.items[index], level));
-        }
-        return items;
-    }
+//URL base del servidor local
+const BASE_URL = 'http://localhost:5009';
 
-    moveLeft() {
-        var newActive = this.active;
-        newActive--;
-        this.active = newActive < 0 ? this.items.length - 1 : newActive;
-        this.direction = 'left';
-        this.render();
-    }
 
-    moveRight() {
-        var newActive = this.active;
-        this.active = (newActive + 1) % this.items.length;
-        this.direction = 'right';
-        this.render();
-    }
-
-    render() {
-        const app = document.getElementById('app');
-        app.innerHTML = `
-            <div id="carousel" class="noselect">
-                <div class="arrow arrow-left" onclick="carousel.leftClick()"><i class="fi-arrow-left"></i></div>
-                ${this.generateItems().map(item => item.render()).join('')}
-                <div class="arrow arrow-right" onclick="carousel.rightClick()"><i class="fi-arrow-right"></i></div>
-            </div>
-        `;
-    }
+// Función para cargar las categorías en el carrusel
+function cargarCategorias() {
+    fetch(`${BASE_URL}/Categoria`)
+        .then(response => response.json())
+        .then(data => {
+            const slickCarousel = $('.slick-carousel');
+            data.forEach(categoria => {
+                slickCarousel.append(`
+                    <div class="card">
+                        <img src="${categoria.imagen}" class="image" />
+                        <section class="body">
+                            <h3 class="titulo-categoria">${categoria.nombre}</h3>
+                            <p class="text">${categoria.descripcion}</p>
+                        </section>
+                    </div>
+                `);
+            });
+            // Configuración del carrusel
+            $('.slick-carousel').slick({
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                focusOnSelect: true,
+                prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+                nextArrow: '<button type="button" class="slick-next">Next</button>'
+            });
+        })
+        .catch(error => console.error('Error al cargar las categorías: ', error));
 }
 
-class Item {
-    constructor(id, level) {
-        this.id = id;
-        this.level = level;
-    }
-
-    render() {
-        const className = 'item level' + this.level;
-        return `
-            <div class="${className}">
-                ${this.id}
-            </div>
-        `;
-    }
-}
-
-const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const carousel = new Carousel(items, 0);
+// Llamada a la función para cargar las categorías
+cargarCategorias();
